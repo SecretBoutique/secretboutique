3de2deb1e54bf7f6030e76424ea9e979c3a441de
GTimothee <ava_punksmash@protonmail.com>
2019-12-16 15:25:21 -0500

    Updates

---
+++ precache-manifest.1518250d4e892ff7778297672fbe0317.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "193cd33c55376148b26ff9408c3e154e",
+    "url": "/secretboutique/index.html"
+  },
+  {
+    "revision": "14f023a499e41317b8cf",
+    "url": "/secretboutique/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0c58141fd5819763e9b1",
+    "url": "/secretboutique/static/js/2.bbc13edf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/secretboutique/static/js/2.bbc13edf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14f023a499e41317b8cf",
+    "url": "/secretboutique/static/js/main.ee48713a.chunk.js"
+  },
+  {
+    "revision": "b2c99e32f6ee06f45b05",
+    "url": "/secretboutique/static/js/runtime-main.47a0d5eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/secretboutique/static/media/logo.5d5d9eef.svg"
+  }
+]);
